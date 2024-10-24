6941bc8faedb6f71465ea15f952226cbb4efb86e
Krysztal Huang <krysztal.huang@outlook.com>
2024-10-25 02:19:14 +0800

    Add LICENSE and also apply file header

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+// Copyright 2024 Krysztal Huang
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 use std::{ops::Deref, time::Duration};
 
 use futures_timer::Delay;
